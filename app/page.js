dae38b11cdfd54ae71e03a92f6229c09b388d9ce
Bharat020195 <tillu0201@gmail.com>
2024-03-23 17:50:39 +0530

    added files

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from '@/components/Header'
-import Header2 from '@/components/Header2'
-import Banner from '@/components/Banner'
-import Apply from '@/components/Apply'
-import Country from '@/components/Country'
-import Services from '@/components/Services'
-import Footer from '@/components/Footer'
+import Header from '../components/Header'
+import Header2 from '../components/Header2'
+import Banner from '../components/Banner'
+import Apply from '../components/Apply'
+import Country from '../components/Country'
+import Services from '../components/Services'
+import Footer from '../components/Footer'
 import './globals.css'
 
 const page = () => {
